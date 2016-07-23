4a7ed41b599fd308aedd9dfc2376a70e361caf9a
lykeven <wangyanzxyq@yahoo.com>
2016-07-23 16:30:32 +0800

    update getSum

--- src/main/scala/getSum.scala
+++ src/main/scala/getSum.scala
@@ -14,7 +14,7 @@ object getSum {
     val conf = new SparkConf().setAppName("WordCount")
     val sc = new SparkContext(conf)
 
-    val input= sc.textFileideai(dirIn)
+    val input= sc.textFile(dirIn)
     val countResult = input.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey(_ + _)
     countResult.collect().foreach(println) // 控制台输出
     countResult.saveAsTextFile(dirOut) // 写入文本文件
