da93f600d45b1da34693b95ace1f466ba8a7e83a
lykeven <wangyanzxyq@yahoo.com>
2016-07-25 22:47:41 +0800

    add cc and update simrank with graphx

--- src/main/scala/SimRank.scala
+++ src/main/scala/SimRank.scala
@@ -3,21 +3,75 @@
   */
 
 import org.apache.spark._
+
 import scala.collection.mutable.ArrayBuffer
 import SparkContext._
+import org.apache.spark.graphx.PartitionStrategy.RandomVertexCut
+import org.apache.spark.graphx._
 import org.apache.spark.rdd.RDD
+
+import scala.reflect.ClassTag
 
 object SimRank {
 
   //change simrank values of (i,i) to 1.0
-  def change(s:(String, Double)): (String, Double) ={
+  def change(s: (String, Double)): (String, Double) = {
     val line = s._1.split('_')
-    if (line(0)==line(1)) (s._1, 1.0)
+    if (line(0) == line(1)) (s._1, 1.0)
     else s
   }
 
+  def change2(s: ((Long, Long), Double)): ((Long, Long), Double) = {
+    if (s._1._1 == s._1._2) (s._1, 1.0)
+    else s
+  }
 
-  def main(args: Array[String]) {
+  def simRankByGraphx[VD: ClassTag, ED: ClassTag](graph: Graph[VD, ED]): RDD[((Long, Long), Double)] = {
+    val g = graph.groupEdges((a, b) => (a)).cache()
+
+    val inDegree = g.inDegrees
+    val inDegreePair = inDegree.cartesian(inDegree).map { case ((vid1, degree1), (vid2, degree2)) =>
+      ((vid1, vid2), degree1 * degree2)
+
+    }.cache()
+
+    val nodeList = g.vertices
+    var simrank = nodeList.cartesian(nodeList).map { case ((vid1, _), (vid2, _)) =>
+      if (vid1 == vid2) ((vid1, vid2), 1.0)
+      else ((vid1, vid2), 0.0)
+    }.cache()
+
+    val outEdges = g.collectNeighborIds(EdgeDirection.Out)
+    val graphPair = outEdges.cartesian(outEdges).flatMap { case ((vid1, nbr1), (vid2, nbr2)) =>
+      var pair = ArrayBuffer[(Long, Long)]()
+      for (x <- nbr1) {
+        for (y <- nbr2) {
+          pair.append((x, y))
+        }
+      }
+      pair.map { f => ((vid1, vid2), f) }
+    }.groupByKey().cache()
+
+    val C = 0.8
+    val iters = 10
+    //iteratively update simrank values
+    for (i <- 1 to iters) {
+      val con = graphPair.join(simrank).values.flatMap { case (urls, sim) =>
+        urls.map(url => (url, sim))
+      }
+      //update simrank values with definition
+      simrank = con.reduceByKey(_ + _).join(inDegreePair).mapValues { s =>
+        if (s._2 > 10e-10) s._1 / s._2 * C
+        else 0.0
+      }
+      simrank = simrank.map(s => change2(s))
+      simrank.foreach(println _)
+    }
+    return simrank
+  }
+
+
+  def simRank() {
 
     val C = 0.8
     val iters = 10
@@ -25,43 +79,55 @@ object SimRank {
     val sc = new SparkContext(conf)
 
     val graphLine = sc.textFile("/home/wangyan/IdeaProjects/test2/in/graphLine.txt")
-    val outEdges = graphLine.map{s=> (s.split(':')(0),s.split(':')(1))}.cache()
+    val outEdges = graphLine.map { s => (s.split(':')(0), s.split(':')(1)) }.cache()
     //node pairs and their outdegree node pairs, which combine with each outdegree nodes of these two nodes
-    val graph = outEdges.cartesian(outEdges).flatMap{s=>
+    val graph = outEdges.cartesian(outEdges).flatMap { s =>
       val line1 = s._1._2.split(',')
       val line2 = s._2._2.split(',')
       val list = ArrayBuffer[String]()
-      for(x <- line1){
-        for(y <- line2){
+      for (x <- line1) {
+        for (y <- line2) {
           list += x + '_' + y
         }
       }
-      list.map{f=> (s._1._1 + '_' + s._2._1, f)}
+      list.map { f => (s._1._1 + '_' + s._2._1, f) }
     }.groupByKey().cache()
 
-    val inDegree = outEdges.map{s=>(s._1, s._2.split(',').length)}
+    val inDegree = outEdges.map { s => (s._1, s._2.split(',').length) }
     //indegree product between two nodes
-    val inDegreePair = inDegree.cartesian(inDegree).map{s=>(s._1._1+'_'+s._2._1, s._1._2*s._2._2)}.cache()
+    val inDegreePair = inDegree.cartesian(inDegree).map { s => (s._1._1 + '_' + s._2._1, s._1._2 * s._2._2) }.cache()
 
     val nodeList = outEdges.keys
     // initialize simrank values
-    var simrank = nodeList.cartesian(nodeList).map{s=>
-      if(s._1==s._2) (s._1 + '_' +s._2, 1.0)
-      else (s._1 + '_' +s._2, 0.0)
+    var simrank = nodeList.cartesian(nodeList).map { s =>
+      if (s._1 == s._2) (s._1 + '_' + s._2, 1.0)
+      else (s._1 + '_' + s._2, 0.0)
     }.cache()
 
     //iteratively update simrank values
-    for (i<- 1 to iters){
-      val con = graph.join(simrank).values.flatMap{case (urls, sim)=>  urls.map(url=>(url,sim))}
+    for (i <- 1 to iters) {
+      val con = graph.join(simrank).values.flatMap { case (urls, sim) => urls.map(url => (url, sim)) }
       //update simrank values with definition
-      simrank = con.reduceByKey(_+_).join(inDegreePair).mapValues{s=>
-        if (s._2 >10e-10) s._1/s._2 * C
+      simrank = con.reduceByKey(_ + _).join(inDegreePair).mapValues { s =>
+        if (s._2 > 10e-10) s._1 / s._2 * C
         else 0.0
       }
-      simrank=simrank.map(s => change(s))
+      simrank = simrank.map(s => change(s))
       simrank.foreach(println _)
     }
     //simrank.saveAsTextFile("/home/wangyan/IdeaProjects/test2/out/simrank")
     sc.stop()
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    val conf = new SparkConf().setAppName("learn").setMaster("local")
+    val sc = new SparkContext(conf)
+
+    val edges = Array((0L, 1L), (0L, 3L),(1L, 2L), (2L, 1L), (2L, 3L) ,(3L, 1L))
+    val rawEdges = sc.parallelize(edges ++ edges)
+    val graph = Graph.fromEdgeTuples(rawEdges, true, uniqueEdges = Some(RandomVertexCut)).cache()
+    val simRankVal = simRankByGraphx(graph)
+
   }
 }
--- src/main/scala/learn.scala
+++ src/main/scala/learn.scala
@@ -4,17 +4,96 @@
 
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.SparkContext._
+import org.apache.spark.graphx.{VertexRDD, _}
+import org.apache.spark.graphx.PartitionStrategy.RandomVertexCut
+import org.apache.spark.rdd.RDD
+
+import scala.reflect.ClassTag
 
 object learn {
+
+  def myCC [VD: ClassTag, ED: ClassTag](graph: Graph[VD,ED]): Graph[Double,ED] = {
+    val g = graph.groupEdges((a, b) => (a)).cache()
+
+    val nbrMaps: VertexRDD[Map[VertexId, Int]] =
+      g.collectNeighborIds(EdgeDirection.Either).mapValues { (vid, nodes) =>
+        var nMap = Map.empty[VertexId, Int]
+        for (i <- 0 to nodes.size-1) {
+          if (nodes(i) != vid)
+            nMap += (nodes(i) -> (nMap.getOrElse(nodes(i), 0) + 1))
+        }
+        nMap
+      }
+
+    val graphNbr: Graph[Map[VertexId, Int], ED] =
+      g.outerJoinVertices(nbrMaps) { (vid, _, nbr) =>
+        nbr.getOrElse(null)
+      }
+
+    def countFunc(ctx: EdgeContext[Map[VertexId, Int], ED, Double]) = {
+      if(ctx.srcId == ctx.dstId) {
+        ctx.sendToSrc(0)
+        ctx.sendToDst(0)
+      }
+      val (smallId, largeId , smallMap, largeMap) =
+        if(ctx.srcId > ctx.dstId) (ctx.srcId, ctx.dstId, ctx.srcAttr, ctx.dstAttr)
+        else (ctx.dstId, ctx.srcId, ctx.dstAttr, ctx.srcAttr)
+      var smallCount ,largeCount = 0
+      var iter = smallMap.iterator
+      while(iter.hasNext){
+        val element = iter.next()
+        val vid = element._1
+        val smallVal = element._2
+        val largeVal = largeMap.getOrElse(vid,0)
+        if(largeVal>0 && vid != ctx.srcId && vid != ctx.dstId){
+          smallCount += smallVal
+          largeCount += largeVal
+        }
+
+      }
+      if(smallId == ctx.srcId){
+        ctx.sendToSrc(smallCount)
+        ctx.sendToDst(largeCount)
+      }else{
+        ctx.sendToSrc(largeCount)
+        ctx.sendToDst(smallCount)
+      }
+    }
+
+    var nbrNum = Map[VertexId, Int]()
+    nbrMaps.collect().foreach { case (vid, nbVal) =>
+      nbrNum += (vid -> nbVal.size)
+    }
+
+    val counts: VertexRDD[Double] = graphNbr.aggregateMessages(countFunc, _ + _)
+
+    val cc = g.outerJoinVertices(counts) { case (vid, _, optCounter: Option[Double]) =>
+      val dblCount: Double = optCounter.getOrElse(0)
+      val nbNum = nbrNum(vid)
+      if (nbNum > 1) {
+        dblCount / (2 * nbNum * (nbNum - 1))
+      }
+      else {
+        0
+      }
+    }
+    return  cc
+  }
+
+
 
   def main(args: Array[String]): Unit = {
 
     val conf = new SparkConf().setAppName("learn").setMaster("local")
     val sc = new SparkContext(conf)
 
-    val data = Array( 1, 2, 3, 4, 5)
-    val distData = sc. parallelize( data)
-    distData.reduce( ( a, b) => a + b)
-    distData.collect().foreach(println)
+    val edges = Array((0L, 1L), (1L, 2L), (2L, 0L))
+    val rawEdges = sc.parallelize(edges ++ edges)
+    val graph =Graph.fromEdgeTuples(rawEdges,true,uniqueEdges =Some(RandomVertexCut)).cache()
+    val ccs = myCC(graph)
+    ccs.vertices.foreach{case (vid, cc)=>
+      println(vid +" : " + cc)
+    }
+
   }
 }
